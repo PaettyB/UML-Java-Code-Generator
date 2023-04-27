a03c1e5e5fe563650f338b2eb39b9455ccae5f9d
Patrick Barclay <patrick.d.barclay@gmail.com>
2023-04-27 14:06:07 +0200

    Button Render

--- src/main/java/de/paettyb/umlEditor/Main.java
+++ src/main/java/de/paettyb/umlEditor/Main.java
@@ -1,27 +1,31 @@
 package de.paettyb.umlEditor;
 
 import java.awt.*;
+
 import de.paettyb.engine.Engine;
 import de.paettyb.umlEditor.ui.Button;
 
 public class Main extends Engine {
+    
+    Button b = new Button("Test", 50, 50, () -> System.out.println("Test"));
+    
     public Main(String name, int width, int height) {
         super(name, width, height);
-        Button b = new Button("Test", 50,50, () -> System.out.println("Test"));
-        b.handleEvent(55,55);
+        b.handleEvent(55, 55);
     }
-
+    
     public static void main(String[] args) {
         Main main = new Main("UML Editor", 1280, 720);
+        main.start();
     }
-
+    
     @Override
     public void tick() {
-
+    
     }
-
+    
     @Override
     public void render(Graphics g) {
-
+        b.render(g);
     }
 }
--- src/main/java/de/paettyb/umlEditor/ui/Button.java
+++ src/main/java/de/paettyb/umlEditor/ui/Button.java
@@ -1,31 +1,106 @@
 package de.paettyb.umlEditor.ui;
 
-public class Button{
+import java.awt.*;
 
-    private int x,y;
+public class Button {
+    
+    private int x, y;
     private int width = 40, height = 20;
     private String label;
-
-    Runnable callback;
-
-    public Button(String label, int x, int y, Runnable callback){
+    private Color color = Color.CYAN;
+    private Font font;
+    private int padding = 10;
+    private int labelWidth = 0, labelHeight = 0;
+    private final Runnable callback;
+    
+    public Button(String label, int x, int y, Runnable callback) {
         this.label = label;
         this.x = x;
         this.y = y;
         this.callback = callback;
+        font = new Font("Arial", Font.PLAIN, 20);
     }
-
-    public boolean handleEvent(int mouseX, int mouseY){
-        if (mouseX >= x && mouseX <= x+width && mouseY >= y && mouseY <= y + height){
+    
+    public void render(Graphics g) {
+        g.setFont(font);
+        if (labelWidth == 0) {
+            labelWidth = g.getFontMetrics().stringWidth(label);
+            labelHeight = g.getFontMetrics().getAscent();
+            width = padding * 2 + labelWidth;
+            height = padding * 2 + labelHeight;
+            System.out.println(width + ", " + height);
+            System.out.println(labelWidth + ", " + labelHeight);
+        }
+        g.setColor(color);
+        g.fillRect(x, y, width, height);
+        
+        g.setColor(Color.BLACK);
+        g.drawString(label, x + padding, y + labelHeight + padding);
+    }
+    
+    public boolean handleEvent(int mouseX, int mouseY) {
+        if (mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height) {
             try {
                 callback.run();
                 return true;
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
         return false;
     }
-
+    
+    public int getX() {
+        return x;
+    }
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+    
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    public String getLabel() {
+        return label;
+    }
+    
+    public void setLabel(String label) {
+        this.label = label;
+        labelWidth = 0;
+        labelHeight = 0;
+    }
+    
+    public Color getColor() {
+        return color;
+    }
+    
+    public void setColor(Color color) {
+        this.color = color;
+    }
+    
+    public Font getFont() {
+        return font;
+    }
+    
+    public void setFont(Font font) {
+        this.font = font;
+        labelWidth = 0;
+        labelHeight = 0;
+    }
+    
+    public int getPadding() {
+        return padding;
+    }
+    
+    public void setPadding(int padding) {
+        this.padding = padding;
+        labelWidth = 0;
+        labelHeight = 0;
+    }
 }
